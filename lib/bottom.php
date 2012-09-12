6f183034670d7f1f1c1bd0dd739b8a4e4cd54198
shamil c m <b090084cs@athena.nitc.ac.in>
2012-09-13 01:13:28 +0530

    few changes in gitignore

---
+++ lib/bottom.php
@@ -0,0 +1,24 @@
+<?php 
+
+function getbottombox(){	
+	echo<<<E_O_T		
+			<div class="bottombox">
+				<div id="bottomline"> </div>
+				<div id="copyright">
+			        <span> WIREUS &#169 2011 </span>
+				</div>
+				
+				
+				<div id="linkbox">
+				<a href="about.php">About</a>
+				 . 
+				<a href="privacy.php">Privacy</a>
+				 . 
+				<a href="help.php">Help</a>
+		        </div>
+	          </div>
+E_O_T
+;
+	          
+	 }
+	 ?>
